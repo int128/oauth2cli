3c80d0b693aac2e5076045d7954474b0611a8bd8
Hidetake Iwata <int128@gmail.com>
2019-05-16 10:16:37 +0900

    Refactor: add test cases on errors

--- authcode_test.go
+++ authcode_test.go
@@ -15,68 +15,206 @@ import (
 )
 
 func TestAuthCodeFlow_GetToken(t *testing.T) {
-	// Start an auth server.
-	h := authServerHandler{
-		t:            t,
-		AuthCode:     "AUTH_CODE",
-		Scope:        "email",
-		AccessToken:  "ACCESS_TOKEN",
-		RefreshToken: "REFRESH_TOKEN",
-	}
-	s := httptest.NewServer(&h)
-	defer s.Close()
-	endpoint := oauth2.Endpoint{
-		AuthURL:  s.URL + "/auth",
-		TokenURL: s.URL + "/token",
-	}
-
-	// Wait for the local server and open a browser request.
-	ctx, cancel := context.WithTimeout(context.TODO(), 1*time.Second)
-	defer cancel()
-	openBrowserCh := make(chan string)
-	defer close(openBrowserCh)
-	go func() {
-		select {
-		case url := <-openBrowserCh:
-			body, err := openBrowserRequest(url)
-			if err != nil {
-				cancel()
-				t.Errorf("Could not open browser request: %+v", err)
-			}
-			t.Logf("got response body: %s", body)
-			if body != oauth2cli.AuthCodeFlowSuccessResponse {
-				t.Errorf("response body did not match")
-			}
-		case <-ctx.Done():
-			t.Errorf("Context done while waiting for opening browser: %+v", ctx.Err())
+	t.Run("Success", func(t *testing.T) {
+		// Start an auth server.
+		h := authServerHandler{
+			t: t,
+			NewAuthResponse: func(scope, state, redirectURI string) string {
+				if w := "email profile"; scope != w {
+					t.Errorf("scope wants %s but %s", w, scope)
+					return fmt.Sprintf("%s?error=invalid_scope", redirectURI)
+				}
+				return fmt.Sprintf("%s?state=%s&code=%s", redirectURI, state, "AUTH_CODE")
+			},
+			NewTokenResponse: func(code string) (int, string) {
+				if w := "AUTH_CODE"; code != w {
+					t.Errorf("code wants %s but %s", w, code)
+					return 400, `{"error":"invalid_grant"}`
+				}
+				return 200, `{"access_token": "ACCESS_TOKEN","token_type": "Bearer","expires_in": 3600,"refresh_token": "REFRESH_TOKEN"}`
+			},
 		}
-	}()
+		s := httptest.NewServer(&h)
+		defer s.Close()
+		endpoint := oauth2.Endpoint{
+			AuthURL:  s.URL + "/auth",
+			TokenURL: s.URL + "/token",
+		}
 
-	// Start a local server and get a token.
-	flow := oauth2cli.AuthCodeFlow{
-		Config: oauth2.Config{
-			ClientID:     "YOUR_CLIENT_ID",
-			ClientSecret: "YOUR_CLIENT_SECRET",
-			Endpoint:     endpoint,
-			Scopes:       []string{"email"},
-		},
-		SkipOpenBrowser: true,
-		ShowLocalServerURL: func(url string) {
-			openBrowserCh <- url
-		},
-		LocalServerMiddleware: loggingMiddleware(t),
-	}
-	token, err := flow.GetToken(ctx)
-	if err != nil {
-		t.Errorf("Could not get a token: %+v", err)
-		return
-	}
-	if h.AccessToken != token.AccessToken {
-		t.Errorf("AccessToken wants %s but %s", h.AccessToken, token.AccessToken)
-	}
-	if h.RefreshToken != token.RefreshToken {
-		t.Errorf("RefreshToken wants %s but %s", h.AccessToken, token.AccessToken)
-	}
+		// Wait for the local server and open a browser request.
+		ctx, cancel := context.WithTimeout(context.TODO(), 1*time.Second)
+		defer cancel()
+		openBrowserCh := make(chan string)
+		defer close(openBrowserCh)
+		go func() {
+			select {
+			case url := <-openBrowserCh:
+				status, body, err := openBrowserRequest(url)
+				if err != nil {
+					cancel()
+					t.Errorf("Could not open browser request: %+v", err)
+				}
+				t.Logf("got response body: %s", body)
+				if status != 200 {
+					t.Errorf("status wants 200 but %d", status)
+				}
+				if body != oauth2cli.AuthCodeFlowSuccessResponse {
+					t.Errorf("response body did not match")
+				}
+			case <-ctx.Done():
+				t.Errorf("Context done while waiting for opening browser: %+v", ctx.Err())
+			}
+		}()
+
+		// Start a local server and get a token.
+		flow := oauth2cli.AuthCodeFlow{
+			Config: oauth2.Config{
+				ClientID:     "YOUR_CLIENT_ID",
+				ClientSecret: "YOUR_CLIENT_SECRET",
+				Endpoint:     endpoint,
+				Scopes:       []string{"email", "profile"},
+			},
+			SkipOpenBrowser: true,
+			ShowLocalServerURL: func(url string) {
+				openBrowserCh <- url
+			},
+			LocalServerMiddleware: loggingMiddleware(t),
+		}
+		token, err := flow.GetToken(ctx)
+		if err != nil {
+			t.Errorf("Could not get a token: %+v", err)
+			return
+		}
+		if "ACCESS_TOKEN" != token.AccessToken {
+			t.Errorf("AccessToken wants %s but %s", "ACCESS_TOKEN", token.AccessToken)
+		}
+		if "REFRESH_TOKEN" != token.RefreshToken {
+			t.Errorf("RefreshToken wants %s but %s", "REFRESH_TOKEN", token.AccessToken)
+		}
+	})
+
+	t.Run("ErrorAuthResponse", func(t *testing.T) {
+		h := authServerHandler{
+			t: t,
+			NewAuthResponse: func(scope, state, redirectURI string) string {
+				return fmt.Sprintf("%s?error=server_error", redirectURI)
+			},
+			NewTokenResponse: func(code string) (int, string) {
+				return 500, "should not reach here"
+			},
+		}
+		s := httptest.NewServer(&h)
+		defer s.Close()
+		endpoint := oauth2.Endpoint{
+			AuthURL:  s.URL + "/auth",
+			TokenURL: s.URL + "/token",
+		}
+
+		// Wait for the local server and open a browser request.
+		ctx, cancel := context.WithTimeout(context.TODO(), 1*time.Second)
+		defer cancel()
+		openBrowserCh := make(chan string)
+		defer close(openBrowserCh)
+		go func() {
+			select {
+			case url := <-openBrowserCh:
+				status, body, err := openBrowserRequest(url)
+				if err != nil {
+					cancel()
+					t.Errorf("Could not open browser request: %+v", err)
+				}
+				t.Logf("got response body: %s", body)
+				if status != 500 {
+					t.Errorf("status wants 500 but %d", status)
+				}
+			case <-ctx.Done():
+				t.Errorf("Context done while waiting for opening browser: %+v", ctx.Err())
+			}
+		}()
+
+		// Start a local server and get a token.
+		flow := oauth2cli.AuthCodeFlow{
+			Config: oauth2.Config{
+				ClientID:     "YOUR_CLIENT_ID",
+				ClientSecret: "YOUR_CLIENT_SECRET",
+				Endpoint:     endpoint,
+				Scopes:       []string{"email", "profile"},
+			},
+			SkipOpenBrowser: true,
+			ShowLocalServerURL: func(url string) {
+				openBrowserCh <- url
+			},
+			LocalServerMiddleware: loggingMiddleware(t),
+		}
+		_, err := flow.GetToken(ctx)
+		if err == nil {
+			t.Errorf("GetToken wants error but nil")
+		}
+		t.Logf("expected error: %s", err)
+	})
+
+	t.Run("ErrorTokenResponse", func(t *testing.T) {
+		h := authServerHandler{
+			t: t,
+			NewAuthResponse: func(scope, state, redirectURI string) string {
+				return fmt.Sprintf("%s?state=%s&code=%s", redirectURI, state, "AUTH_CODE")
+			},
+			NewTokenResponse: func(code string) (int, string) {
+				return 400, `{"error":"invalid_request"}`
+			},
+		}
+		s := httptest.NewServer(&h)
+		defer s.Close()
+		endpoint := oauth2.Endpoint{
+			AuthURL:  s.URL + "/auth",
+			TokenURL: s.URL + "/token",
+		}
+
+		// Wait for the local server and open a browser request.
+		ctx, cancel := context.WithTimeout(context.TODO(), 1*time.Second)
+		defer cancel()
+		openBrowserCh := make(chan string)
+		defer close(openBrowserCh)
+		go func() {
+			select {
+			case url := <-openBrowserCh:
+				status, body, err := openBrowserRequest(url)
+				if err != nil {
+					cancel()
+					t.Errorf("Could not open browser request: %+v", err)
+				}
+				t.Logf("got response body: %s", body)
+				if status != 200 {
+					t.Errorf("status wants 200 but %d", status)
+				}
+				if body != oauth2cli.AuthCodeFlowSuccessResponse {
+					t.Errorf("response body did not match")
+				}
+			case <-ctx.Done():
+				t.Errorf("Context done while waiting for opening browser: %+v", ctx.Err())
+			}
+		}()
+
+		// Start a local server and get a token.
+		flow := oauth2cli.AuthCodeFlow{
+			Config: oauth2.Config{
+				ClientID:     "YOUR_CLIENT_ID",
+				ClientSecret: "YOUR_CLIENT_SECRET",
+				Endpoint:     endpoint,
+				Scopes:       []string{"email", "profile"},
+			},
+			SkipOpenBrowser: true,
+			ShowLocalServerURL: func(url string) {
+				openBrowserCh <- url
+			},
+			LocalServerMiddleware: loggingMiddleware(t),
+		}
+		_, err := flow.GetToken(ctx)
+		if err == nil {
+			t.Errorf("GetToken wants error but nil")
+		}
+		t.Logf("expected error: %s", err)
+	})
 }
 
 func loggingMiddleware(t *testing.T) func(h http.Handler) http.Handler {
@@ -88,28 +226,30 @@ func loggingMiddleware(t *testing.T) func(h http.Handler) http.Handler {
 	}
 }
 
-func openBrowserRequest(url string) (string, error) {
+func openBrowserRequest(url string) (int, string, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		return "", errors.Wrapf(err, "could not send a request")
+		return 0, "", errors.Wrapf(err, "could not send a request")
 	}
 	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		return "", errors.Errorf("status wants 200 but %d", resp.StatusCode)
-	}
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", errors.Wrapf(err, "could not read response body")
+		return resp.StatusCode, "", errors.Wrapf(err, "could not read response body")
 	}
-	return string(b), nil
+	return resp.StatusCode, string(b), nil
 }
 
 type authServerHandler struct {
-	t            *testing.T
-	Scope        string
-	AuthCode     string
-	AccessToken  string
-	RefreshToken string
+	t *testing.T
+
+	// This should return a URL with query parameters of authorization response.
+	// See https://tools.ietf.org/html/rfc6749#section-4.1.2
+	NewAuthResponse func(scope, state, redirectURI string) string
+
+	// This should return a JSON body of access token response or error response.
+	// See https://tools.ietf.org/html/rfc6749#section-5.1
+	// and https://tools.ietf.org/html/rfc6749#section-5.2
+	NewTokenResponse func(code string) (int, string)
 }
 
 func (h *authServerHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -134,10 +274,7 @@ func (h *authServerHandler) serveHTTP(w http.ResponseWriter, r *http.Request) er
 		if redirectURI == "" {
 			return errors.New("redirect_uri is missing")
 		}
-		if h.Scope != scope {
-			return errors.Errorf("scope wants %s but %s", h.Scope, scope)
-		}
-		to := fmt.Sprintf("%s?state=%s&code=%s", redirectURI, state, h.AuthCode)
+		to := h.NewAuthResponse(scope, state, redirectURI)
 		http.Redirect(w, r, to, 302)
 
 	case r.Method == "POST" && r.URL.Path == "/token":
@@ -152,18 +289,11 @@ func (h *authServerHandler) serveHTTP(w http.ResponseWriter, r *http.Request) er
 		if redirectURI == "" {
 			return errors.New("redirect_uri is missing")
 		}
-		if h.AuthCode != code {
-			return errors.Errorf("code wants %s but %s", h.AuthCode, code)
-		}
+		status, b := h.NewTokenResponse(code)
 		w.Header().Add("Content-Type", "application/json")
-		b := fmt.Sprintf(`{
-			"access_token": "%s",
-			"token_type": "Bearer",
-			"expires_in": 3600,
-			"refresh_token": "%s"
-		}`, h.AccessToken, h.RefreshToken)
+		w.WriteHeader(status)
 		if _, err := w.Write([]byte(b)); err != nil {
-			return errors.Wrapf(err, "error while writing body")
+			return errors.Wrapf(err, "error while writing response body")
 		}
 
 	default:
