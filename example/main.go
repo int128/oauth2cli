74a6a5f63cfb46e23247549ec1ac50fc9cfbfbec
Hidetake Iwata <int128@gmail.com>
2019-08-30 20:48:31 +0900

    Fix example to wait for all goroutines

--- example/main.go
+++ example/main.go
@@ -8,45 +8,51 @@ import (
 	"github.com/int128/oauth2cli"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
+	"golang.org/x/sync/errgroup"
+	"golang.org/x/xerrors"
 )
 
 func main() {
+	ctx := context.Background()
 	clientID, clientSecret := os.Getenv("GOOGLE_CLIENT_ID"), os.Getenv("GOOGLE_CLIENT_SECRET")
 	if clientID == "" || clientSecret == "" {
 		log.Fatalf("You need to set GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET")
 	}
 
-	ctx := context.Background()
-
-	// open the browser on ready (this is optional)
-	ready := make(chan string)
-	defer close(ready)
-	go func() {
+	ready := make(chan string, 1)
+	var eg errgroup.Group
+	eg.Go(func() error {
 		select {
 		case url, ok := <-ready:
-			if ok {
-				log.Printf("Open %s", url)
-			} else {
-				log.Printf("channel has been closed while waiting for authorization")
+			if !ok {
+				return nil
 			}
+			log.Printf("Open %s", url)
+			// you can open the browser here
+			return nil
 		case err := <-ctx.Done():
-			log.Printf("context done while waiting for authorization: %s", err)
+			return xerrors.Errorf("context done while waiting for authorization: %w", err)
 		}
-	}()
-
-	// perform authorization
-	token, err := oauth2cli.GetToken(ctx, oauth2cli.Config{
-		OAuth2Config: oauth2.Config{
-			ClientID:     clientID,
-			ClientSecret: clientSecret,
-			Endpoint:     google.Endpoint,
-			Scopes:       []string{"email"},
-		},
-		LocalServerReadyChan: ready,
 	})
-	if err != nil {
-		log.Fatalf("Could not get a token: %s", err)
+	eg.Go(func() error {
+		defer close(ready)
+		token, err := oauth2cli.GetToken(ctx, oauth2cli.Config{
+			OAuth2Config: oauth2.Config{
+				ClientID:     clientID,
+				ClientSecret: clientSecret,
+				Endpoint:     google.Endpoint,
+				Scopes:       []string{"email"},
+			},
+			LocalServerReadyChan: ready,
+		})
+		if err != nil {
+			return xerrors.Errorf("could not get a token: %w", err)
+		}
+		log.Printf("Got a token: %+v", token)
+		log.Printf("Your token is valid until %s", token.Expiry)
+		return nil
+	})
+	if err := eg.Wait(); err != nil {
+		log.Printf("error while authorization: %s", err)
 	}
-	log.Printf("Got a token: %+v", token)
-	log.Printf("Your token is valid until %s", token.Expiry)
 }
