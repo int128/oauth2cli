15adf1bc07682d067593ad2a4ab58f5075281a58
Hidetake Iwata <int128@gmail.com>
2019-09-03 10:19:57 +0900

    Fix for taking screencast

--- example/main.go
+++ example/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/int128/oauth2cli"
+	"github.com/pkg/browser"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"golang.org/x/sync/errgroup"
@@ -16,7 +17,11 @@ func main() {
 	ctx := context.Background()
 	clientID, clientSecret := os.Getenv("GOOGLE_CLIENT_ID"), os.Getenv("GOOGLE_CLIENT_SECRET")
 	if clientID == "" || clientSecret == "" {
-		log.Fatalf("You need to set GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET")
+		log.Fatalf(`You need to set oauth2 credentials.
+Open https://console.cloud.google.com/apis/credentials and create a client. And then,
+export GOOGLE_CLIENT_ID=xxx.apps.googleusercontent.com
+export GOOGLE_CLIENT_SECRET=xxx
+`)
 	}
 
 	ready := make(chan string, 1)
@@ -28,7 +33,9 @@ func main() {
 				return nil
 			}
 			log.Printf("Open %s", url)
-			// you can open the browser here
+			if err := browser.OpenURL(url); err != nil {
+				log.Printf("could not open the browser: %s", err)
+			}
 			return nil
 		case err := <-ctx.Done():
 			return xerrors.Errorf("context done while waiting for authorization: %w", err)
@@ -48,8 +55,7 @@ func main() {
 		if err != nil {
 			return xerrors.Errorf("could not get a token: %w", err)
 		}
-		log.Printf("Got a token: %+v", token)
-		log.Printf("Your token is valid until %s", token.Expiry)
+		log.Printf("You got a valid token until %s", token.Expiry)
 		return nil
 	})
 	if err := eg.Wait(); err != nil {
