ec48308c884b36dc8883ea4a3673314f99eb19e3
Hidetake Iwata <int128@gmail.com>
2019-10-03 00:09:33 +0900

    Refactor: introduce flag to example app (#10)

--- example/README.md
+++ example/README.md
@@ -10,27 +10,20 @@ Setup your API access:
 1. Open https://console.cloud.google.com/apis/credentials
 1. Create an OAuth client ID where the application type is other.
 
-Set the environment variables:
-
-```sh
-export GOOGLE_CLIENT_ID=xxx.apps.googleusercontent.com
-export GOOGLE_CLIENT_SECRET=xxx
-```
-
-You can set a certificate for the local server.
-
-```sh
-export LOCAL_SERVER_CERT=testdata/cert.pem
-export LOCAL_SERVER_KEY=testdata/cert-key.pem
-```
-
-Run the application:
+Build and run the application:
 
 ```
-% go run main.go
-2019/05/15 09:39:16 Open http://localhost:58806
-2019/05/15 09:39:26 Got a token: &{AccessToken:... TokenType:Bearer RefreshToken:... scope:https://www.googleapis.com/auth/userinfo.email openid token_type:Bearer]}
-2019/05/15 09:39:26 Your token is valid until 2019-05-15 10:39:26.720292 +0900 JST m=+3609.800317611
+% go build
+% ./example -client-id xxx.apps.googleusercontent.com -client-secret xxxxxxxx
+2019/10/03 00:01:35 Open http://localhost:53753
+2019/10/03 00:01:40 You got a valid token until 2019-10-03 01:01:40.083238 +0900 JST m=+3604.526750517
 ```
 
 It will automatically open the browser and you can log in to Google.
+
+As well as you can set a certificate and key for the local server.
+
+```
+% ./example -client-id xxx.apps.googleusercontent.com -client-secret xxxxxxxx \
+    -local-server-cert ../testdata/cert.pem -local-server-key ../testdata/cert-key.pem
+```
--- example/main.go
+++ example/main.go
@@ -2,32 +2,44 @@ package main
 
 import (
 	"context"
-	"log"
-	"os"
-
+	"flag"
 	"github.com/int128/oauth2cli"
 	"github.com/pkg/browser"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"golang.org/x/sync/errgroup"
 	"golang.org/x/xerrors"
+	"log"
+	"os"
 )
 
+type cmdOptions struct {
+	clientID        string
+	clientSecret    string
+	localServerCert string
+	localServerKey  string
+}
+
 func main() {
-	ctx := context.Background()
-	clientID, clientSecret := os.Getenv("GOOGLE_CLIENT_ID"), os.Getenv("GOOGLE_CLIENT_SECRET")
-	if clientID == "" || clientSecret == "" {
-		log.Fatalf(`You need to set oauth2 credentials.
-Open https://console.cloud.google.com/apis/credentials and create a client. And then,
-export GOOGLE_CLIENT_ID=xxx.apps.googleusercontent.com
-export GOOGLE_CLIENT_SECRET=xxx
-`)
+	var o cmdOptions
+	flag.StringVar(&o.clientID, "client-id", "", "OAuth Client ID")
+	flag.StringVar(&o.clientSecret, "client-secret", "", "OAuth Client Secret")
+	flag.StringVar(&o.localServerCert, "local-server-cert", "", "Path to a certificate file for the local server (optional)")
+	flag.StringVar(&o.localServerKey, "local-server-key", "", "Path to a key file for the local server (optional)")
+	flag.Parse()
+	if o.clientID == "" || o.clientSecret == "" {
+		log.Printf(`You need to set oauth2 credentials.
+Open https://console.cloud.google.com/apis/credentials and create a client.
+Then set the following options:`)
+		flag.PrintDefaults()
+		os.Exit(1)
+		return
 	}
-	localServerCert, localServerKey := os.Getenv("LOCAL_SERVER_CERT"), os.Getenv("LOCAL_SERVER_KEY")
-	if localServerCert != "" {
-		log.Printf("Using a TLS certificate: %s", localServerCert)
+	if o.localServerCert != "" {
+		log.Printf("Using a TLS certificate: %s", o.localServerCert)
 	}
 
+	ctx := context.Background()
 	ready := make(chan string, 1)
 	var eg errgroup.Group
 	eg.Go(func() error {
@@ -49,14 +61,14 @@ export GOOGLE_CLIENT_SECRET=xxx
 		defer close(ready)
 		token, err := oauth2cli.GetToken(ctx, oauth2cli.Config{
 			OAuth2Config: oauth2.Config{
-				ClientID:     clientID,
-				ClientSecret: clientSecret,
+				ClientID:     o.clientID,
+				ClientSecret: o.clientSecret,
 				Endpoint:     google.Endpoint,
 				Scopes:       []string{"email"},
 			},
 			LocalServerReadyChan: ready,
-			LocalServerCertFile:  localServerCert,
-			LocalServerKeyFile:   localServerKey,
+			LocalServerCertFile:  o.localServerCert,
+			LocalServerKeyFile:   o.localServerKey,
 		})
 		if err != nil {
 			return xerrors.Errorf("could not get a token: %w", err)
