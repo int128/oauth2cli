63794961607a735900f7bfe97b5be84a94d0ec29
Hidetake Iwata <int128@gmail.com>
2020-02-22 12:29:05 +0900

    Always use PKCE in example app

--- example/pkce.go
+++ example/pkce.go
@@ -5,36 +5,35 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/binary"
-	"log"
-	"strings"
 
-	"github.com/int128/oauth2cli"
-	"golang.org/x/oauth2"
+	"golang.org/x/xerrors"
 )
 
-func configurePKCE(cfg *oauth2cli.Config) {
-	b := make([]byte, 32)
-	if err := binary.Read(rand.Reader, binary.LittleEndian, b); err != nil {
-		log.Fatalf("error: %s", err)
+func generateChallengeAndVerifier() (string, string, error) {
+	b, err := random32()
+	if err != nil {
+		return "", "", xerrors.Errorf("could not generate PKCE parameters: %w", err)
 	}
 	codeChallenge, codeVerifier := computeChallengeAndVerifier(b)
-	cfg.AuthCodeOptions = []oauth2.AuthCodeOption{
-		oauth2.SetAuthURLParam("code_challenge_method", "S256"),
-		oauth2.SetAuthURLParam("code_challenge", codeChallenge),
-	}
-	cfg.TokenRequestOptions = []oauth2.AuthCodeOption{
-		oauth2.SetAuthURLParam("code_verifier", codeVerifier),
-	}
+	return codeChallenge, codeVerifier, nil
 }
 
 func computeChallengeAndVerifier(b []byte) (string, string) {
-	verifier := base64urlencode(b)
+	verifier := base64URLEncode(b)
 	s := sha256.New()
 	_, _ = s.Write([]byte(verifier))
-	challenge := base64urlencode(s.Sum(nil))
+	challenge := base64URLEncode(s.Sum(nil))
 	return challenge, verifier
 }
 
-func base64urlencode(b []byte) string {
-	return strings.TrimRight(base64.URLEncoding.EncodeToString(b), "=")
+func random32() ([]byte, error) {
+	b := make([]byte, 32)
+	if err := binary.Read(rand.Reader, binary.LittleEndian, b); err != nil {
+		return nil, xerrors.Errorf("read error: %w", err)
+	}
+	return b, nil
+}
+
+func base64URLEncode(b []byte) string {
+	return base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString(b)
 }
